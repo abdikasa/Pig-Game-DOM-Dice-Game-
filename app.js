bcc1dba60084dff4156f96c4cd6f0e067a8c3374
Said-Abdikarim <kareemfrmto@gmail.com>
2019-05-23 16:54:46 -0400

    Setting up the game
    Added IFEE, wrote basic code/template for the project.

--- app.js
+++ app.js
@@ -9,6 +9,45 @@ GAME RULES:
 
 */
 
+//Define variabeles
+let activePlayer, scores, roundScore, stillPlaying, dice;
+
+activePlayer = 0;
+scores = [0, 0];
+roundScore = 0;
+stillPlaying = true;
+
+//DOM Handlers
+let hold = document.querySelector(".btn-hold");
+let roll = document.querySelector(".btn-roll");
+let newGame = document.querySelector(".btn-new");
+let p1 = document.querySelector("#score-0");
+let p2 = document.querySelector("#score-1");
+let p1_curr = document.querySelector("#current-0");
+let p2_curr = document.querySelector("#current-1");
+
+
+
+
+// IFEE Reset Game to Zero
+(function () {
+    p1.textContent = 0;
+    p2.textContent = 0;
+    p1_curr.textContent = 0;
+    p2_curr.textContent = 0;
+})()
+
+
+//Commented for later use
+//dice = Math.floor(Math.random() * 6) + 1;
+
+//Functions here
+
+
+
+
+
+
 
 
 
